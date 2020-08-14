e6282cb1eb32e39f025dbf6498ed83a5d2270247
Tahsin Abrar <abrar.cste@gmail.com>
2020-08-15 02:48:23 +0600

    hospital and patient analysis done

--- resources/views/iedcr/hospital-and-patient-analysis.blade.php
+++ resources/views/iedcr/hospital-and-patient-analysis.blade.php
@@ -1,9 +1,20 @@
 @extends('iedcr.default')
-@section('bread_crumb_active','Conformance Analysis')
+@section('bread_crumb_active','Hospital and Patient Analysis')
 @section('content')
     <?php
     ini_set('error_reporting', 0);
+    $_hospitalCapacityDataSet = $_deathLocationPercentageDataSet = NULL;
+    $_hospitalCapacityDataSet = \Illuminate\Support\Facades\DB::table('hpa_hospital_capacity')->get();
+    $_deathLocationPercentageDataLabels = array('Location','Percentage(%)');
+    $_deathLocationPercentageDataSet = array();
+    $_deathLocInfoData = \Illuminate\Support\Facades\DB::table('hpa_death_location_percent')->get();
+    if(count($_deathLocInfoData)) {
+        foreach ($_deathLocInfoData as $key=>$_deathInfo){
+            $_deathLocationPercentageDataSet[$key+1][0] = $_deathInfo->location;
+            $_deathLocationPercentageDataSet[$key+1][1] = $_deathInfo->percentage;
 
+        }
+    }
     ?>
     <!-- Top Row -->
     <div class="row">
@@ -20,20 +31,41 @@
                         <table id="case_analysis_dtable" class="table table-striped table-bordered text-nowrap">
                             <thead>
                             <tr>
-                                <?php foreach($_hospitalCapacityDataLabels as $_tableHead):?>
-                                <th class="border-bottom-0"><?php echo $_tableHead; ?></th>
-                                <?php endforeach;?>
+                            <tr>
+                                <th class="border-bottom-0">Facility Name</th>
+                                <th class="border-bottom-0">General Bed</th>
+                                <th class="border-bottom-0">Usable ICU Bed</th>
+                                <th class="border-bottom-0">Ventilator</th>
+                                <th class="border-bottom-0">General Bed</th>
+                                <th class="border-bottom-0">ICU Bed</th>
+                                <th class="border-bottom-0">Admitted Patient Last 24 Hours</th>
+                                <th class="border-bottom-0">Released Patient Last 24 Hours</th>
+                                <th class="border-bottom-0">Oxygen Cylinder #</th>
+                                <th class="border-bottom-0">High flow nasal cannula #</th>
+                                <th class="border-bottom-0">Oxygen Concentrator #</th>
+                                <th class="border-bottom-0">Central Oxygen Line</th>
+
                             </tr>
                             </thead>
                             <tbody>
-                            <?php foreach($_hospitalCapacityDataSet as $_rowKey => $_rowDataSet):?>
-                            <?php if($_rowKey == 0 || ((count($_hospitalCapacityDataSet)-1) == $_rowKey) || ((count($_hospitalCapacityDataSet)-2) == $_rowKey)) continue; ?>
-                            <tr>
-                                <?php foreach($_rowDataSet as $_columnData):?>
-                                <td><?php echo ($_columnData == NULL)?"-":$_columnData; ?></td>
-                                <?php endforeach;?>
-                            </tr>
-                            <?php endforeach;?>
+                            @if(count($_hospitalCapacityDataSet))
+                                @foreach($_hospitalCapacityDataSet as $key=>$_hospitalData)
+                                    <tr>
+                                        <td>{!! $_hospitalData->facility_name ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->general_bed ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->usable_ICU_bed ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->ventilator ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->general_bed_2 ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->ICU_bed ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->admitted_patient_last_24 ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->released_patient_last_24 ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->oxygen_cylinder ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->high_flow_nasal_cannula ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->oxygen_concentrator ?? ' ' !!}</td>
+                                        <td>{!! $_hospitalData->central_oxygen_line ?? ' ' !!}</td>
+                                    </tr>
+                                @endforeach
+                            @endif
                             </tbody>
                         </table>
                     </div>
@@ -296,6 +328,58 @@
 
 @section('scripts')
 
-
+    <script type="text/javascript">
+        /* Death Location Percentage */
+        <?php
+        $_deathLocationPercentageData = array();
+        foreach($_deathLocationPercentageDataSet as $_rowKey => $_rowData){
+            if($_rowKey == 0) continue;
+            $_deathLocationPercentageData[] = array('name' => $_rowData[0], 'y' => (float)number_format($_rowData[1],2));
+        }
+        ?>
+        Highcharts.chart('death-location-percentage', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: '',
+                y:0
+            },
+            credits:{
+                enabled:false
+            },
+            legend:{
+                enabled:true,
+                labelFormatter: function () {
+                    return this.name+': <b> '+this.y + '%</b>';
+                }
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false,
+                        formatter:function(){
+                            return this.key+ ': ' + this.y + '%';
+                        }
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: 'Death Percentage',
+                colorByPoint: true,
+                innerSize: '70%',
+                data: <?php echo json_encode($_deathLocationPercentageData); ?>
+            }]
+        });
+    </script>
 
 @endsection
