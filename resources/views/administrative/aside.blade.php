8ca51d70e04bf47eba72ee61b0a9b0fd06397994
Tahsin Abrar <abrar.cste@gmail.com>
2020-08-17 10:16:28 +0600

    add permission in cabinet aside files

--- resources/views/administrative/aside.blade.php
+++ resources/views/administrative/aside.blade.php
@@ -3,19 +3,39 @@
     <div class="alert alert-primary small last-updated-date" role="alert"> <strong><strong>শেষ আপডেট: ১১/০৮/২০২০</strong></strong> </div>
     <ul class="nav">
     <li class="nav-item"> <a class="nav-link" href="dashboard.php"> <span class="menu-title">সামগ্রিক চিত্র</span> <i class="mdi mdi-home menu-icon"></i> </a> </li>
+    @can('cabinet-risk-zone-report')
     <li class="nav-item"> <a class="nav-link" href="risk-zone-iframe.html"> <span class="menu-title">রিস্ক জোন রিপোর্ট</span> <i class="mdi mdi-chart-bar menu-icon"></i> </a> </li>
+    @endcan
+
+    @can('cabinet-obility-of-affected-people')
     <li class="nav-item"> <a class="nav-link" href="#"> <span class="menu-title">আক্রান্ত ব্যাক্তির চলাচল</span> <i class="mdi mdi-nature-people menu-icon"></i> </a> </li>
+    @endcan
+
+    @can('cabinet-mobility-of-citizen')
     <li class="nav-item"> <a class="nav-link" href="#"> <span class="menu-title">নাগরিক চলাচল</span> <i class="mdi mdi-pac-man menu-icon"></i> </a> </li>
+    @endcan
+
+    @can('cabinet-synoptic-surveillance-map')
     <li class="nav-item"> <a class="nav-link" href="#"> <span class="menu-title">সিনোপটিক সারভেইল্যান্স ম্যাপ</span> <i class="mdi mdi-map menu-icon"></i> </a> </li>
+    @endcan
+
+    @can('cabinet-civil-surgeon-report')
     <li class="nav-item"> <a class="nav-link" href="#"> <span class="menu-title">সিভিল সার্জন রিপোর্ট</span> <i class="mdi mdi-chart-bar menu-icon"></i> </a> </li>
+    @endcan
     <!-- <li class="nav-item">
             <a class="nav-link" href="#">
                 <span class="menu-title">এলার্ট সার্ভিস মনিটরিং</span>
                 <i class="mdi mdi-information menu-icon"></i>
             </a>
         </li>-->
+
+    @can('cabinet-social-distance-analysis')
     <li class="nav-item"> <a class="nav-link" href="dataframe.php?datatype=sdp_monitoring"> <span class="menu-title">সামাজিক দুরত্ব পর্যবেক্ষণ</span> <i class="mdi mdi-eye menu-icon"></i> </a> </li>
+    @endcan
+    
+    @can('cabinet-deep-analysis')
     <li class="nav-item"> <a class="nav-link" href="dataframe.php?datatype=analytics"> <span class="menu-title">এনালিটিক্স (বিশ্লেষণ)</span> <i class="mdi mdi-chart-arc menu-icon"></i> </a> </li>
+    @endcan
 
     @can('dashboard-switch')
     <li class="slide"> <a class="side-menu__item" href="{!! route('iedcr.dashboard') !!}"> <svg class="side-menu__icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
