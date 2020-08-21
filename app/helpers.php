472904587539f1c6390464127bb97dcb0948b009
Tahsin Abrar <abrar.cste@gmail.com>
2020-08-21 16:14:05 +0600

    row5 functionality has done

--- app/helpers.php
+++ app/helpers.php
@@ -17,3 +17,31 @@ if (! function_exists('en2bnTranslation')) {
         }
     }
 }
+
+if (! function_exists('mapDivisionColor')) {
+    /**
+     * English to bangla translation
+     */
+    function mapDivisionColor($division_value, $division_name, $filter_division)
+    {
+        if($filter_division != '' && $division_name != strtolower($filter_division)){
+            $color = '#FCAA94';
+        }else{
+            if($division_value <= 200){
+                $color = '#F69475';
+            }elseif($division_value > 201 && $division_value <= 500){
+                $color = '#F37366';
+            }elseif($division_value > 501 && $division_value <= 800){
+                $color = '#E5515D';
+            }elseif($division_value > 801 && $division_value <= 1000){
+                $color = '#CD3E52';
+            }elseif($division_value > 1000){
+                $color = '#BC2B4C';
+            }else{
+                $color = '#FCAA94';
+            }
+        }
+
+        return $color;
+    }
+}
