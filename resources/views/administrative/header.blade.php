1b8771a5e54198029c33e37ea40ff1f8851f8795
Bagdoom.com <benzir.hasan@bagdoom.com>
2020-09-08 22:09:37 +0600

    Administrative Dashboard New UI Change

--- resources/views/administrative/header.blade.php
+++ resources/views/administrative/header.blade.php
@@ -1,24 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-<!-- Required meta tags -->
-<meta charset="utf-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<title>National Dashboard</title>
-<!-- plugins:css -->
-<link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}"/>
-<!-- endinject -->
-<!-- Plugin css for this page -->
-<!-- End plugin css for this page -->
-<!-- inject:css -->
-<!-- endinject -->
-<!-- Layout styles -->
-<link href="https://fonts.maateen.me/solaiman-lipi/font.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('assets/css/style_administrative.css') }}"/>
-<!-- End layout styles -->
-<!--<link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />-->
-<!-- Custom Style -->
-<link rel="stylesheet" href="{{ asset('assets/css/custom_administrative.css') }}"/>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<!-- Meta data -->
+<meta charset="UTF-8">
+<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+<meta content="National Dashboard - IEDCR Version" name="description">
+<meta content="eGenration Limited" name="author">
+<meta name="keywords" content=""/>
+
+<!-- Title -->
+<title>জাতীয় ড্যাশবোর্ড</title>
+
+<!--Favicon -->
+<link rel="icon" href="{{ asset('assets/images/brand/favicon.ico') }}" type="image/x-icon"/>
+
+<!--Bootstrap css -->
+<link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+<!-- Style css -->
+<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/dark.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/skin-modes.css') }}" rel="stylesheet" />
+
+<!-- Animate css -->
+<link href="{{ asset('assets/css/animated.css') }}" rel="stylesheet" />
+
+<!--Sidemenu css -->
+<link href="{{ asset('assets/css/sidemenu.css') }}" rel="stylesheet">
+
+<!-- P-scroll bar css-->
+<link href="{{ asset('assets/plugins/p-scrollbar/p-scrollbar.css') }}" rel="stylesheet" />
+
+<!---Icons css-->
+<link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" />
+
+<!-- Simplebar css -->
+<link rel="stylesheet" href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}">
+
+<!-- INTERNAL Select2 css -->
+<link href="{{ asset('assets/plugins/select2/select2.min.css') }}" rel="stylesheet" />
+
+<!-- INTERNAL C3 Charts css-->
+<link href="{{ asset('assets/plugins/charts-c3/c3-chart.css') }}" rel="stylesheet" />
+
+<!-- Color Skin css -->
+<link id="theme" href="{{ asset('assets/colors/color1.css') }}" rel="stylesheet" type="text/css"/>
+
+<!-- Custom CSS by eGen -->
+<link id="theme" href="{{ asset('assets/css/custom_administrative.css?v=6') }}" rel="stylesheet" type="text/css"/>
+<style>
+	.fill_color{
+		fill: #705ec8;
+	}
+	.stroke_color{
+		stroke:#705ec8;
+	}
+</style>
 </head>
---
+++ resources/views/administrative/search_view.blade.php
@@ -0,0 +1,169 @@
+@php
+    $divisions = DB::table('upazila')->distinct()->get('division');
+@endphp
+
+
+    <div class="form-group mb-0 mt-3">
+        <div class="custom-controls-stacked d-flex">
+            <label class="custom-control custom-radio mr-2">
+                <input type="radio" class="custom-control-input hierarchy_level natioanl_level" name="hierarchy_level"
+                       value="national" @php if (request()->get('hierarchy_level') != 'divisional') { echo "checked"; } @endphp>
+                <span class="custom-control-label">জাতীয় পর্যায়</span> </label>
+            <label class="custom-control custom-radio mr-2">
+                <input type="radio" class="custom-control-input hierarchy_level divisional_level" name="hierarchy_level"
+                       value="divisional" @php if (request()->get('hierarchy_level') == 'divisional') { echo "checked"; } @endphp>
+                <span class="custom-control-label">বিভাগীয় পর্যায়</span> </label>
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body p-0">
+            <div class="btn-group mt-2 mb-2 mr-1">
+                <select class="btn btn-outline-primary dropdown-toggle division" name="division" id="">
+                    <option value="">
+                        সব বিভাগ
+                    </option>
+                    @foreach($divisions as $division)
+                        <option @if(request()->get('division') == $division->division) selected @endif value="{{$division->division}}"><?php echo en2bnTranslation($division->division); ?></option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="btn-group mt-2 mb-2 mr-1">
+                <select class="btn btn-outline-primary dropdown-toggle district" name="district" id="">
+
+                    @if(request()->has('district') && request()->get('district') != '')
+                        <option value="{{ request()->get('district') }}">{{ request()->get('district') }}</option>
+                    @endif
+                    <option value="">সব জেলা</option>
+
+
+
+                </select>
+            </div>
+            <div class="btn-group mt-2 mb-2 mr-1">
+                <select class="btn btn-outline-primary dropdown-toggle upazilla" name="upazila" id="">
+                    <option value="">
+                        সব উপজেলা
+                    </option>
+                </select>
+            </div>
+            <div class="btn-group">
+                <div class="col-md-6 pl-0 d-none">
+                    <input class="form-control" placeholder="From Date" type="date" name="from_date" value="{{ request()->get('from_date') }}">
+                </div>
+                <div class="col-md-6 pl-0 d-none">
+                    <input class="form-control" placeholder="To Date" type="date" name="to_date" value="{{ request()->get('to_date') }}">
+                </div>
+                <div class="btn-group">
+                    <button class="btn btn-primary-color pl-0" type="submit">
+                        <svg class="header-icon search-icon" x="1008" y="1248" viewBox="0 0 24 24"
+                             height="100%" width="100%" preserveAspectRatio="xMidYMid meet"
+                             focusable="false">
+                            <path d="M0 0h24v24H0V0z" fill="none"/>
+                            <path
+                                d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+@push('custom_script')
+    <script>
+        function htmlEntities(str) {
+            return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+        }
+
+        $('.division').change(function (e) {
+            var division = $(".division").val();
+            // console.log(division);
+            $.ajax({
+                type: 'GET',
+                url: '{{route('get-district-from-division')}}',
+                dataType: 'json',
+                data: {division: division},
+                success: function (data) {
+                    console.log(data);
+                    if (data.status == 1) {
+                        $('.district').empty();
+                        html = '<option value="">জেলা</option>';
+                        for (var i = 0; i < data.data.length; i++) {
+                            var selectedDistrict = "{{ htmlentities(request()->get('district')) }}";
+                            var selectedValue = '';
+                            if (selectedDistrict == htmlEntities(data.data[i].district)) {
+                                selectedValue = 'selected';
+                            }
+                            html += '<option ' + selectedValue +' value="' + htmlEntities(data.data[i].district) + '">' + data.data[i].district + '</option>';
+                        }
+                        $('.district').append(html);
+                    }
+                },
+                error: function (data) {
+
+                }
+            });
+
+        });
+
+        $('.district').change(function (e) {
+            var district = $(".district").val();
+            // console.log(district);
+            $.ajax({
+                type: 'GET',
+                url: '{{route('get-upazilla-from-district')}}',
+                dataType: 'json',
+                data: {district: district},
+                success: function (data) {
+                    console.log(data);
+
+                    if (data.status == 1) {
+                        $('.upazilla').empty();
+                        html = '<option value="">উপজেলা</option>';
+                        for (var i = 0; i < data.data.length; i++) {
+                            var selectedUpazilla = '{{ htmlentities(request()->get('upazila')) }}';
+                            var selectedValue = '';
+                            if (selectedUpazilla == htmlEntities(data.data[i].upazila_en)) {
+                                selectedValue = 'selected';
+                            }
+                            html += '<option ' + selectedValue + ' value="' + htmlEntities(data.data[i].upazila_en) + '">' + data.data[i].upazila_en + '</option>';
+                        }
+                        $('.upazilla').append(html);
+                    }
+                },
+                error: function (data) {
+
+                }
+            });
+
+        });
+        $('.natioanl_level').on('click', function () {
+            $('.division').prop('selectedIndex',0);
+            $('.district').prop('selectedIndex',0);
+            $('.upazilla').prop('selectedIndex',0);
+            $('.division').prop('disabled', 'disabled');
+            $('.district').prop('disabled', 'disabled');
+            $('.upazilla').prop('disabled', 'disabled');
+        });
+
+        $('.divisional_level').on('click', function () {
+            $('.division').prop('disabled', false);
+            $('.district').prop('disabled', false);
+            $('.upazilla').prop('disabled', false);
+        });
+
+        if($('.divisional_level').is(':checked')) {
+            $('.division').prop('disabled', false);
+            $('.district').prop('disabled', false);
+            $('.upazilla').prop('disabled', false);
+        } else {
+            $('.division').prop('selectedIndex',0);
+            $('.district').prop('selectedIndex',0);
+            $('.upazilla').prop('selectedIndex',0);
+            $('.division').prop('disabled', 'disabled');
+            $('.district').prop('disabled', 'disabled');
+            $('.upazilla').prop('disabled', 'disabled');
+        }
+    </script>
+@endpush
