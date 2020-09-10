a8b4b379a234e22cfbb88caeef0677f6fea582cf
shamvilhossain <shamvil89@gmail.com>
2020-09-10 13:43:10 +0600

    test positivity map update

--- app/Http/Controllers/iedcr/IedcrDashboardController.php
+++ app/Http/Controllers/iedcr/IedcrDashboardController.php
@@ -589,23 +589,38 @@ class IedcrDashboardController extends Controller
         }
 
         if($searchQuery != '') {
-            $testPositivityMapSql = "select A.District, (A.Positive/B.Total)*100 as 'Test_Positivity' from
-(select district as 'District', max(date_of_test) as 'last_date', count(id) as 'Positive'
-from lab_clean_data
-where test_result='Positive' ". $searchQuery."  group by district) as A
-inner join
-(select district as 'District', max(date_of_test) as 'last_date', count(id) as 'Total'
-from lab_clean_data
-where test_result='Positive' or test_result='Negative' group by district) as B using(District)";
+//             $testPositivityMapSql = "select A.District, (A.Positive/B.Total)*100 as 'Test_Positivity' from
+// (select district as 'District', max(date_of_test) as 'last_date', count(id) as 'Positive'
+// from lab_clean_data
+// where test_result='Positive' ". $searchQuery."  group by district) as A
+// inner join
+// (select district as 'District', max(date_of_test) as 'last_date', count(id) as 'Total'
+// from lab_clean_data
+// where test_result='Positive' or test_result='Negative' group by district) as B using(District)";
+
+            $testPositivityMapSql = "SELECT T2.district AS District, SUM(T1.test_positivity)/COUNT(test_positivity) AS 'Test_Positivity' FROM
+(SELECT A.bbs_code AS 'bbs_code', (A.Positive/B.Total)*100 AS 'test_positivity' FROM
+(SELECT bbs_code, COUNT(id) AS 'Positive'
+FROM lab_clean_data
+WHERE test_result='Positive' ". $searchQuery." GROUP BY bbs_code) AS A
+INNER JOIN 
+(SELECT bbs_code, COUNT(id) AS 'Total'
+FROM lab_clean_data
+WHERE test_result='Positive' OR test_result='Negative' GROUP BY bbs_code) AS B 
+USING(bbs_code)) AS T1 INNER JOIN bbs_coded_upazila_dist_div AS T2 
+ON T1.bbs_code=T2.upz_code GROUP BY T2.district";
         } else {
-            $testPositivityMapSql = "select A.District, (A.Positive/B.Total)*100 as 'Test_Positivity' from
-(select district as 'District', max(date_of_test) as 'last_date', count(id) as 'Positive'
-from lab_clean_data
-where test_result='Positive' group by district) as A
-inner join
-(select district as 'District', max(date_of_test) as 'last_date', count(id) as 'Total'
-from lab_clean_data
-where test_result='Positive' or test_result='Negative' group by district) as B using(District)";
+            $testPositivityMapSql = "SELECT T2.district AS District, SUM(T1.test_positivity)/COUNT(test_positivity) AS 'Test_Positivity' FROM
+(SELECT A.bbs_code AS 'bbs_code', (A.Positive/B.Total)*100 AS 'test_positivity' FROM
+(SELECT bbs_code, COUNT(id) AS 'Positive'
+FROM lab_clean_data
+WHERE test_result='Positive' GROUP BY bbs_code) AS A
+INNER JOIN 
+(SELECT bbs_code, COUNT(id) AS 'Total'
+FROM lab_clean_data
+WHERE test_result='Positive' OR test_result='Negative' GROUP BY bbs_code) AS B 
+USING(bbs_code)) AS T1 INNER JOIN bbs_coded_upazila_dist_div AS T2 
+ON T1.bbs_code=T2.upz_code GROUP BY T2.district";
         }
 
         $testMapData = \Illuminate\Support\Facades\DB::select($testPositivityMapSql);
