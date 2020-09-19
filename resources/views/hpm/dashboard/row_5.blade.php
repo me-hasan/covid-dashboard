f9a420b042ef8a5190d38ca5f02166aee485ce98
shamvilhossain <shamvil89@gmail.com>
2020-09-19 21:21:44 +0600

    upazila removed

--- resources/views/hpm/dashboard/row_5.blade.php
+++ resources/views/hpm/dashboard/row_5.blade.php
@@ -21,55 +21,55 @@
 @push('custom_script')
     <script>
         // Divition Wise Two Weeks Change
-        Highcharts.chart('divition_wise_two_weeks_change', {
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: ''
-            },
-            subtitle: {
-                text: ''
-            },
-            credits:{
-                enabled:false
-            },
-            legend:{
-                enabled:true
-            },
-            yAxis: {
-                title: {
-                    text: 'Percentage'
-                },
-                labels: {
-                    formatter: function() {
-                        return this.value+"%";
-                    }
-                },
-                max: 100
-            },
-            xAxis: {
-                categories: ["Dhaka","Khulna","Barisal","Sylhet","Mymensingh","Rajshahi","Rangpur","Chittagong"]
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.y}%</b>'
-            },
-            plotOptions: {
-                column: {
-                    pointPadding: 0.2,
-                    borderWidth: 0
-                }
-            },
-            colors: ['#ef4b4b', '#38cb89'],
-            series: [{
-                name: 'COVID',
-                data: [49, 71, 76, 29, 44, 76, 35, 48]
+        // Highcharts.chart('divition_wise_two_weeks_change', {
+        //     chart: {
+        //         type: 'column'
+        //     },
+        //     title: {
+        //         text: ''
+        //     },
+        //     subtitle: {
+        //         text: ''
+        //     },
+        //     credits:{
+        //         enabled:false
+        //     },
+        //     legend:{
+        //         enabled:true
+        //     },
+        //     yAxis: {
+        //         title: {
+        //             text: 'Percentage'
+        //         },
+        //         labels: {
+        //             formatter: function() {
+        //                 return this.value+"%";
+        //             }
+        //         },
+        //         max: 100
+        //     },
+        //     xAxis: {
+        //         categories: ["Dhaka","Khulna","Barisal","Sylhet","Mymensingh","Rajshahi","Rangpur","Chittagong"]
+        //     },
+        //     tooltip: {
+        //         pointFormat: '{series.name}: <b>{point.y}%</b>'
+        //     },
+        //     plotOptions: {
+        //         column: {
+        //             pointPadding: 0.2,
+        //             borderWidth: 0
+        //         }
+        //     },
+        //     colors: ['#ef4b4b', '#38cb89'],
+        //     series: [{
+        //         name: 'COVID',
+        //         data: [49, 71, 76, 29, 44, 76, 35, 48]
 
-            }, {
-                name: 'Non-Covid',
-                data: [83, 78, 98, 93, 56, 84, 55]
+        //     }, {
+        //         name: 'Non-Covid',
+        //         data: [83, 78, 98, 93, 56, 84, 55]
 
-            }]
-        });
+        //     }]
+        // });
     </script>
 @endpush
