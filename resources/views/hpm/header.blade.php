084f5f3799f8ca66b496fc8f541930606b78f4d2
shamvilhossain <shamvil89@gmail.com>
2020-09-21 00:13:36 +0600

    new files

---
+++ resources/views/hpm/header.blade.php
@@ -0,0 +1,62 @@
+
+<head>
+
+<!-- Meta data -->
+<meta charset="UTF-8">
+<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+<meta content="National Dashboard - IEDCR Version" name="description">
+<meta content="eGenration Limited" name="author">
+<meta name="keywords" content=""/>
+
+<!-- Title -->
+<title>National Dashboard - IEDCR Version</title>
+
+<!--Favicon -->
+<link rel="icon" href="{{ asset('assets/images/brand/favicon.ico') }}" type="image/x-icon"/>
+
+<!--Bootstrap css -->
+<link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+<!-- Style css -->
+<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/dark.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/skin-modes.css') }}" rel="stylesheet" />
+
+<!-- Animate css -->
+<link href="{{ asset('assets/css/animated.css') }}" rel="stylesheet" />
+
+<!--Sidemenu css -->
+<link href="{{ asset('assets/css/sidemenu.css') }}" rel="stylesheet">
+
+<!-- P-scroll bar css-->
+<link href="{{ asset('assets/plugins/p-scrollbar/p-scrollbar.css') }}" rel="stylesheet" />
+
+<!---Icons css-->
+<link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" />
+
+<!-- Simplebar css -->
+<link rel="stylesheet" href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}">
+
+<!-- INTERNAL Select2 css -->
+<link href="{{ asset('assets/plugins/select2/select2.min.css') }}" rel="stylesheet" />
+
+<!-- INTERNAL C3 Charts css-->
+<link href="{{ asset('assets/plugins/charts-c3/c3-chart.css') }}" rel="stylesheet" />
+
+<!-- Color Skin css -->
+<link id="theme" href="{{ asset('assets/colors/color1.css') }}" rel="stylesheet" type="text/css"/>
+<style>
+@import url('https://fonts.maateen.me/solaiman-lipi/font.css');
+</style>
+<!-- Custom CSS by eGen -->
+<link id="theme" href="{{ asset('assets/css/custom.css?v=5') }}" rel="stylesheet" type="text/css"/>
+    <style>
+        .fill_color{
+            fill: #705ec8;
+        }
+        .stroke_color{
+            stroke:#705ec8;
+        }
+    </style>
+
+</head>
