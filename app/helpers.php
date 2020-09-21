d078824971cb42ba97e225233146de91f3aa9b37
Tahsin Abrar <abrar.cste@gmail.com>
2020-09-21 14:56:15 +0600

    Fix hospital info pie chart showing issue

--- app/helpers.php
+++ app/helpers.php
@@ -50,10 +50,11 @@ if (! function_exists('mapDivisionColor')) {
         $castDate = date("j F", strtotime($getDate));
         $engDATE = array('1','2','3','4','5','6','7','8','9','0','January','February','March','April',
         'May','June','July','August','September','October','November','December','Saturday','Sunday',
-        'Monday','Tuesday','Wednesday','Thursday','Friday');
+        'Monday','Tuesday','Wednesday','Thursday','Friday', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
         $bangDATE = array('১','২','৩','৪','৫','৬','৭','৮','৯','০','জানুয়ারী','ফেব্রুয়ারী','মার্চ','এপ্রিল','মে',
         'জুন','জুলাই','আগস্ট','সেপ্টেম্বর','অক্টোবর','নভেম্বর','ডিসেম্বর','শনিবার','রবিবার','সোমবার','মঙ্গলবার','
-        বুধবার','বৃহস্পতিবার','শুক্রবার'
+        বুধবার','বৃহস্পতিবার','শুক্রবার', 'জানুয়ারী','ফেব্রুয়ারী','মার্চ','এপ্রিল','মে',
+        'জুন','জুলাই','আগস্ট','সেপ্টেম্বর','অক্টোবর','নভেম্বর','ডিসেম্বর'
         );
         $convertedDATE = str_replace($engDATE, $bangDATE, $castDate);
         return $convertedDATE;
