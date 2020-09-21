90aa3a3618c85c06a933784b9c9aabf1c40fb61f
Bagdoom.com <benzir.hasan@bagdoom.com>
2020-09-21 15:54:28 +0600

    Update Chart and Fonts

--- resources/views/hpm/dashboard/row_4.blade.php
+++ resources/views/hpm/dashboard/row_4.blade.php
@@ -3459,7 +3459,7 @@
         <div class="col-xl-8 col-lg-8 col-md-8 col-xm-12">
             <div class="card-body">
                 <h5 class="card-title b1">বর্ণনা</h5>
-                <p class="card-text">  {{ $des_10->description_beng }}</p>
+                <p class="card-text b1"> {{ $des_10->description_beng }}</p>
             </div>
         </div>
         <div class="col-xl-4 col-lg-4 col-md-4 col-xm-12">
