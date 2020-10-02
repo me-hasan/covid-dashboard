6694cd5a17952e25a1219f9fc28f116b9062f618
Bagdoom.com <benzir.hasan@bagdoom.com>
2020-10-02 22:02:29 +0600

    Component Title :: Update

--- resources/views/hpm/dashboard.blade.php
+++ resources/views/hpm/dashboard.blade.php
@@ -47,7 +47,7 @@
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content modal-content-demo">
                 <div class="modal-header">
-                    <h6 class="modal-title"></h6><button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+                    <h6 class="modal-title b1"></h6><button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
                 </div>
                 <div class="modal-body" id="modalContent">
                 </div>
