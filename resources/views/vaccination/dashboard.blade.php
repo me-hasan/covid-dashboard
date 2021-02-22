41613b8f91cbfceaf1789924707790344a5e385f
khayrul <khayrul.web@gmail.com>
2021-02-22 15:13:08 +0600

    Update last commit

--- resources/views/vaccination/dashboard.blade.php
+++ resources/views/vaccination/dashboard.blade.php
@@ -800,6 +800,12 @@ if (isset($last_14_days['getLast14DaysDeathData'][0]->Difference) && $last_14_da
                             {{-- map end here --}}
 
 
+
+
+                            
+                            
+
+
                             {{-- matix start here --}}
                             
                             
@@ -935,7 +941,7 @@ if (isset($last_14_days['getLast14DaysDeathData'][0]->Difference) && $last_14_da
                                 <div class="row">
                                     <div class="iv-left col-6 ">
                                         <h2>
-                                            জেলা দ্বারা ভ্যাকসিন গ্রহণযোগ্যতা জরিপ  
+                                            জেলা ভিত্তিতে ভ্যাকসিন গ্রহণযোগ্যতা জরিপ  
                                         </h2>
                                     </div>
                                     <div class="iv-right offset-md-4 col-2 " style="display: none">
@@ -9344,7 +9350,7 @@ $ydata = [];
     
 
     function showNationalLevelAgeWiseDeathChart(chartData) {
-        // console.log(chartData);
+        console.log(chartData);
         var size = Object.keys(chartData).length;
         var div_date = new Date(chartData[size - 1].date).toLocaleDateString('bn', options);
         // $('#last_date_4').html(" " + div_date);
@@ -9712,191 +9718,7 @@ $ydata = [];
     $('#iframeData_3').html('<iframe id="rtIframeData" scrolling="no" width="100%" style="margin:0px !important; padding:0px !important" height="870px" src="https://public.tableau.com/views/COVIDtestpositivityratedistrictmonth3_16088073716630/Dashboard1?%3Aembed=y&amp;%3AshowVizHome=no" style="overflow-y: hidden" frameborder="0" allowFullScreen="true"></iframe>');
     $('#iframeData_4').html('<iframe id="rtIframeData" scrolling="no" width="100%" style="margin:0px !important; padding:0px !important" height="870px" src="https://public.tableau.com/views/COVIDtestpositivityratedistrictmonth4_16088075184360/Dashboard1?%3Aembed=y&amp;%3AshowVizHome=no" style="overflow-y: hidden" frameborder="0" allowFullScreen="true"></iframe>');
 
-    $(document).ready(function ($) {
-        var slider = document.getElementById("myRange");
-        var slider = document.getElementById("myRange");
-        var output = document.getElementById("demo");
-        //  output.innerHTML = englishToBangla(slider.value);
-
-        slider.oninput = function () {
-            output.innerHTML = englishToBangla(this.value);
-        }
-
-        //$("#ex16b").slider({ min: 10, max: 100, value: [10, 100], labelledby: ['ex18-label-2a', 'ex18-label-2b'], focus: true });
-        $("#ex12c").slider({id: "slider12c", min: 0, max: 30, range: true, value: [5, 10]});
-
-        $("#ex12c").on("slide", function (slideEvt) {
-            $("#ex6SliderVal").text(englishToBangla(slideEvt.value[0]) + ',' + englishToBangla(slideEvt.value[1]));
-            myrange_ajax_call();
-        });
-        myrange_ajax_call();
-
-
-        $('#myRange').on('click', function () {
-            myrange_ajax_call();
-        });
-
-
-        $('#weekly_date_submit').on('click', function () {
-            myrange_ajax_call();
-            weekly_date_change();
-        });
-
-        function weekly_date_change(){
-            let url = new URL('{!! route('weekly.date.change.for.matrix') !!}');
-            $.ajax({
-
-                type: "GET",
-                url: url.toString(),
-                data: {
-                    'weekly_date': $('#weekly_date').val(),
-                },
-                timeout: 30000,
-                success: function (data) {
-                    if (data.status == 'success') {
-                        $('#recent_weekly_date').html(data.recent_weekly_date)
-                        $('#last_weekly_date').html(data.last_weekly_date)
-                    } else {
-                        alert("Something Went Wrong");
-                    }
-                },
-                error: function (request, status, error) {
-                    console.log("Request Param");
-                    // console.log(request.responseText);
-                    console.log("Status Param");
-                    console.log(status);
-                    console.log(error);
-                }
-            });
-            return false;
-        }
-
-
-        function myrange_ajax_call() {
-            // console.log(hpm.getRiskMatricData);
-            let result;
-            let url = new URL('{!! route('hpm.getRiskMatricData') !!}');
-            $.ajax({
-
-                type: "GET",
-                url: url.toString(),
-                data: {
-                    'test_count': $('#myRange').val(),
-                    'test_positive_data_rate': $('#ex12c').val(),
-                    'weekly_date': $('#weekly_date').val(),
-                },
-                timeout: 30000,
-                success: function (data) {
-                    if (data.status == 'success') {
-
-                        rangeChange(data.result_data, data.risk_matrix_data);
-                    } else {
-                        alert("Something Went Wrong");
-                    }
-                },
-                error: function (request, status, error) {
-                    console.log("Request Param");
-                    // console.log(request.responseText);
-                    console.log("Status Param");
-                    console.log(status);
-                    console.log(error);
-                }
-            });
-            return false;
-        }
-
-        function rangeChange(data, risk_matrix_data) {
-            // console.log(risk_matrix_data);
-
-            $('.high_to_high_modal_click').html('<strong>অপরিবর্তিত উচ্চ ঝুঁকি</strong> <br><u>'+englishToBangla(data.high_to_high) + ' টি জেলা</u> <br>' + risk_matrix_data.high_to_high_district_name);
-            $('.high_to_low_modal_click').html('<strong>উচ্চ ঝুঁকি থেকে কম ঝুঁকি</strong> <br><u>'+englishToBangla(data.high_to_low) + ' টি জেলা</u> <br>' + risk_matrix_data.high_to_low_district_name);
-            $('.high_to_medium_modal_click').html('<strong>উচ্চ ঝুঁকি থেকে মধ্যম ঝুঁকি</strong> <br><u>'+englishToBangla(data.high_to_medium) + ' টি জেলা</u> <br>' + risk_matrix_data.high_to_medium_district_name);
-            $('.low_to_high_modal_click').html('<strong>কম ঝুঁকি থেকে উচ্চ ঝুঁকি</strong> <br><u>'+englishToBangla(data.low_to_high) + ' টি জেলা</u> <br>' + risk_matrix_data.low_to_high_district_name);
-            $('.low_to_low_modal_click').html('<strong>অপরিবর্তিত কম ঝুঁকি</strong> <br> <u>'+englishToBangla(data.low_to_low) + ' টি জেলা</u> <br>' + risk_matrix_data.low_to_low_district_name);
-            $('.medium_to_high_modal_click').html('<strong>মধ্যম ঝুঁকি থেকে উচ্চ ঝুঁকি</strong><br><u>'+englishToBangla(data.medium_to_high) + ' টি জেলা</u> <br>' + risk_matrix_data.medium_to_high_district_name);
-            $('.medium_to_low_modal_click').html('<strong>মধ্যম ঝুঁকি থেকে কম ঝুঁকি</strong> <br><u>'+englishToBangla(data.medium_to_low) + ' টি জেলা</u> <br>' + risk_matrix_data.medium_to_low_district_name);
-            $('.medium_to_medium_modal_click').html('<strong>অপরিবর্তিত মধ্যম ঝুঁকি</strong> <br> <u>'+englishToBangla(data.medium_to_medium) + ' টি জেলা</u> <br>' + risk_matrix_data.medium_to_medium_district_name);
-            $('.low_to_medium_modal_click').html('<strong>কম ঝুঁকি থেকে মধ্যম ঝুঁকি</strong> <br> <u>'+englishToBangla(data.low_to_medium) + ' টি জেলা</u> <br>' + risk_matrix_data.low_to_medium_district_name);
-            /*modal data*/
-            $('#high_to_high_table_content tbody').html(risk_matrix_data.high_to_high_table_contentData);
-            $('#medium_to_high_table_content tbody').html(risk_matrix_data.medium_to_high_table_contentData);
-            $('#low_to_high_table_content tbody').html(risk_matrix_data.low_to_high_table_contentData);
-            $('#high_to_medium_table_content tbody').html(risk_matrix_data.high_to_medium_table_contentData);
-            $('#medium_to_medium_table_content tbody').html(risk_matrix_data.medium_to_medium_table_contentData);
-            $('#low_to_medium_table_content tbody').html(risk_matrix_data.low_to_medium_table_contentData);
-            $('#high_to_low_table_content tbody').html(risk_matrix_data.high_to_low_table_contentData);
-            $('#medium_to_low_table_content tbody').html(risk_matrix_data.medium_to_low_table_contentData);
-            $('#low_to_low_table_content tbody').html(risk_matrix_data.low_to_low_table_contentData);
-            /*district data*/
-            $('.high_to_high_district').html(risk_matrix_data.high_to_high_district_name);
-            $('.high_to_low_district').html(risk_matrix_data.high_to_low_district_name);
-            $('.high_to_medium_district').html(risk_matrix_data.high_to_medium_district_name);
-            $('.low_to_high_district').html(risk_matrix_data.low_to_high_district_name);
-            $('.low_to_medium_district').html(risk_matrix_data.low_to_medium_district_name);
-            $('.low_to_low_district').html(risk_matrix_data.low_to_low_district_name);
-            $('.medium_to_high_district').html(risk_matrix_data.medium_to_high_district_name);
-            $('.medium_to_medium_district').html(risk_matrix_data.medium_to_medium_district_name);
-            $('.medium_to_low_district').html(risk_matrix_data.medium_to_low_district_name);
-
-
-        }
-
-        $('.high_to_high_modal_click').click(function () {
-            $('.modal-title').html('ঝুঁকি পর্যালোচনা');
-            $('#modalContent').html($('#high_to_high_table_content').html());
-            //hospitalDataModal();
-
-        });
-
-        $('.medium_to_high_modal_click').click(function () {
-            $('.modal-title').html('ঝুঁকি পর্যালোচনা');
-            $('#modalContent').html($('#medium_to_high_table_content').html());
-            //hospitalDataModal();
-        });
-
-        $('.low_to_high_modal_click').click(function () {
-            $('.modal-title').html('ঝুঁকি পর্যালোচনা');
-            $('#modalContent').html($('#low_to_high_table_content').html());
-            //hospitalDataModal();
-        });
-
-        $('.high_to_medium_modal_click').click(function () {
-            $('.modal-title').html('ঝুঁকি পর্যালোচনা');
-            $('#modalContent').html($('#high_to_medium_table_content').html());
-            //hospitalDataModal();
-        });
-
-        $('.medium_to_medium_modal_click').click(function () {
-            $('.modal-title').html('ঝুঁকি পর্যালোচনা');
-            $('#modalContent').html($('#medium_to_medium_table_content').html());
-            //hospitalDataModal();
-        });
-
-        $('.low_to_medium_modal_click').click(function () {
-            $('.modal-title').html('ঝুঁকি পর্যালোচনা');
-            $('#modalContent').html($('#low_to_medium_table_content').html());
-            //hospitalDataModal();
-        });
-
-        $('.high_to_low_modal_click').click(function () {
-            $('.modal-title').html('ঝুঁকি পর্যালোচনা');
-            $('#modalContent').html($('#high_to_low_table_content').html());
-            //hospitalDataModal();
-        });
-
-        $('.medium_to_low_modal_click').click(function () {
-            $('.modal-title').html('ঝুঁকি পর্যালোচনা');
-            $('#modalContent').html($('#medium_to_low_table_content').html());
-            //hospitalDataModal();
-        });
-
-        $('.low_to_low_modal_click').click(function () {
-            $('.modal-title').html('ঝুঁকি পর্যালোচনা');
-            $('#modalContent').html($('#low_to_low_table_content').html());
-            //hospitalDataModal();
-        });
-
-    });
+ 
 
      /*==============================End============================================
     * টেস্ট পজিটিভিটি রেটের ভিত্তিতে জেলা পর্যায়ে ঝুঁকি বিশ্লেষণs
@@ -10877,115 +10699,9 @@ group by date ORDER BY date ");
 
      // word map
 
-    /**
-    * ---------------------------------------
-    * This demo was created using amCharts 4.
-    * 
-    * For more information visit:
-    * https://www.amcharts.com/
-    * 
-    * Documentation is available at:
-    * https://www.amcharts.com/docs/v4/
-    * ---------------------------------------
-    */
 
-    // Themes begin
-    am4core.useTheme(am4themes_animated);
-    // Themes end
-
-    var continents = {
-      "AF": 0,
-      "AN": 1,
-      "AS": 2,
-      "EU": 3,
-      "NA": 4,
-      "OC": 5,
-      "SA": 6
-    }
-
-    // Create map instance
-    var chart = am4core.create("worldmap", am4maps.MapChart);
-    chart.projection = new am4maps.projections.Miller();
-
-    // Create map polygon series for world map
-    var worldSeries = chart.series.push(new am4maps.MapPolygonSeries());
-    worldSeries.useGeodata = true;
-    worldSeries.geodata = am4geodata_worldLow;
-    worldSeries.exclude = ["AQ"];
-
-    var worldPolygon = worldSeries.mapPolygons.template;
-    worldPolygon.tooltipText = "{name}";
-    worldPolygon.nonScalingStroke = true;
-    worldPolygon.strokeOpacity = 0.5;
-    worldPolygon.fill = am4core.color("#eee");
-    worldPolygon.propertyFields.fill = "color";
-
-    var hs = worldPolygon.states.create("hover");
-    hs.properties.fill = chart.colors.getIndex(9);
-
-
-    // Create country specific series (but hide it for now)
-    var countrySeries = chart.series.push(new am4maps.MapPolygonSeries());
-    countrySeries.useGeodata = true;
-    countrySeries.hide();
-    countrySeries.geodataSource.events.on("done", function(ev) {
-      worldSeries.hide();
-      countrySeries.show();
-    });
-
-    var countryPolygon = countrySeries.mapPolygons.template;
-    countryPolygon.tooltipText = "{name}";
-    countryPolygon.nonScalingStroke = true;
-    countryPolygon.strokeOpacity = 0.5;
-    countryPolygon.fill = am4core.color("#eee");
-
-    var hs = countryPolygon.states.create("hover");
-    hs.properties.fill = chart.colors.getIndex(9);
-
-    // Set up click events
-    worldPolygon.events.on("hit", function(ev) {
-      ev.target.series.chart.zoomToMapObject(ev.target);
-      var map = ev.target.dataItem.dataContext.map;
-      if (map) {
-        ev.target.isHover = false;
-        countrySeries.geodataSource.url = "https://www.amcharts.com/lib/4/geodata/json/" + map + ".json";
-        countrySeries.geodataSource.load();
-      }
-    });
-
-    // Set up data for countries
-    var data = [];
-    for(var id in am4geodata_data_countries2) {
-      if (am4geodata_data_countries2.hasOwnProperty(id)) {
-        var country = am4geodata_data_countries2[id];
-        if (country.maps.length) {
-          data.push({
-            id: id,
-            color: chart.colors.getIndex(continents[country.continent_code]),
-            map: country.maps[0]
-          });
-        }
-      }
-    }
-    worldSeries.data = data;
-
-    // Zoom control
-    chart.zoomControl = new am4maps.ZoomControl();
-
-    var homeButton = new am4core.Button();
-    homeButton.events.on("hit", function() {
-      worldSeries.show();
-      countrySeries.hide();
-      chart.goHome();
-    });
-
-    homeButton.icon = new am4core.Sprite();
-    homeButton.padding(7, 5, 7, 5);
-    homeButton.width = 30;
-    homeButton.icon.path = "M16,8 L14,8 L14,16 L10,16 L10,10 L6,10 L6,16 L2,16 L2,8 L0,8 L8,0 L16,8 Z M16,8";
-    homeButton.marginBottom = 10;
-    homeButton.parent = chart.zoomControl;
-    homeButton.insertBefore(chart.zoomControl.plusButton);
+    
+    
 
 </script>
 
