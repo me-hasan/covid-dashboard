476664367207a8fecf98cc4baceda691cbd0d971
khayrul <khayrul.web@gmail.com>
2021-05-10 16:28:37 +0600

    Mail Update

--- app/Http/Controllers/NewsBulletinController.php
+++ app/Http/Controllers/NewsBulletinController.php
@@ -19,6 +19,11 @@ use Illuminate\Support\Facades\Storage;
 use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Support\Facades\Auth;
 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 class NewsBulletinController extends Controller
 {
     /**
@@ -737,8 +742,46 @@ class NewsBulletinController extends Controller
     public function sendMail($toEmails, $ccEmail, $data2, $pdf)
     {
        
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'mail.dashboard.corona.gov.bd';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'newsletter@dashboard.corona.gov.bd';                     //SMTP username
+            $mail->Password   = 'xWRo7gf9';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 25;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            // dd($mail);
+            //Recipients
+            $mail->setFrom('newsletter@dashboard.corona.gov.bd', 'Mailer');
+            $mail->addAddress('khayrul.web@gmail.com', 'Joe User');     //Add a recipient
+            //$mail->addAddress('Md Khayrul Hasan');               //Name is optional
+            //$mail->addReplyTo('newsletter@dashboard.corona.gov.bd', 'Information');
+            //$mail->addCC('khayrul.web@gmail.com');
+            //$mail->addBCC('bcc@example.com');
+            
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            
+            //dd('dd');
+            $mail->send();
+            //echo 'Message has been sent';
+            dd('dd');
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
             //dispatch(new SendEmailJob($email, $data2, $pdf)); // for queue job
-            Mail::to($toEmails)->cc($ccEmail)->send(new HpmDashboardMail($data2, $pdf));
+            //Mail::to($toEmails)->cc($ccEmail)->send(new HpmDashboardMail($data2, $pdf));
        
 
     }
