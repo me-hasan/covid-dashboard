9140f01332bba04716a15fefd44286ddc5174dbc
khayrul <khayrul.web@gmail.com>
2022-01-09 19:02:47 +0600

    First matrix add to combind dashboard

--- routes/web.php
+++ routes/web.php
@@ -350,3 +350,7 @@ Route::get('xpm-third-risk-matrix-date-change-public', 'combinded\DashboardContr
 Route::get('all-table-data-public', 'combinded\DashboardController@getAllTableData')->name('get.table.data.public');
 Route::get('table-date-change', 'combinded\DashboardController@getRiskMatrixDateChange')->name('weekly.date.change.for.table');
 Route::get('card-infected-data', 'combinded\DashboardController@getCardInfectedData');
+
+
+Route::get('matix', 'combinded\DashboardController@matrix')->name('combinded.matix');
+Route::get('webportal-risk-matrix-data-public', 'combinded\MatrixController@getRiskMatricData')->name('webportal.getRiskMatricData.public');
